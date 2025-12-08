b30403d2a6756a3830e80b6b56b0882ae8f84681
Victor Ramirez <vicdeveloper83@gmail.com>
2025-12-08 19:05:39 +0000

    pagination started

--- store/views.py
+++ store/views.py
@@ -1,27 +1,40 @@
-from django.shortcuts import get_object_or_404, render
-
-from category.models import Category
+from django.shortcuts import render, get_object_or_404, redirect
+# from .models import Product, ReviewRating, ProductGallery
 from .models import Product
-# Create your views here.
+from category.models import Category
+from carts.models import CartItem
+from django.db.models import Q
+
+from carts.views import _cart_id
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+from django.http import HttpResponse
+# from .forms import ReviewForm
+from django.contrib import messages
+# from orders.models import OrderProduct
 
 
 def store(request, category_slug=None):
     categories = None
     products = None
 
-    if category_slug is not None:
+    if category_slug != None:
         categories = get_object_or_404(Category, slug=category_slug)
         products = Product.objects.filter(
             category=categories, is_available=True)
+        paginator = Paginator(products, 1)
+        page = request.GET.get('page')
+        paged_products = paginator.get_page(page)
         product_count = products.count()
     else:
-        products = Product.objects.all().filter(is_available=True)
+        products = Product.objects.all().filter(is_available=True).order_by('id')
+        paginator = Paginator(products, 6)
+        page = request.GET.get('page')
+        paged_products = paginator.get_page(page)
         product_count = products.count()
 
     context = {
-        'products': products,
-        'products_count': product_count
-
+        'products': paged_products,
+        'product_count': product_count,
     }
     return render(request, 'store/store.html', context)
 
