85c1e6a5d0101ddd8755b07b34fcd3a60f986b01
Victor Ramirez <vicdeveloper83@gmail.com>
2025-12-09 16:25:53 +0000

    variation model created

--- store/views.py
+++ store/views.py
@@ -21,7 +21,7 @@ def store(request, category_slug=None):
         categories = get_object_or_404(Category, slug=category_slug)
         products = Product.objects.filter(
             category=categories, is_available=True)
-        paginator = Paginator(products, 1)
+        paginator = Paginator(products, 6)
         page = request.GET.get('page')
         paged_products = paginator.get_page(page)
         product_count = products.count()
@@ -75,18 +75,18 @@ def product_detail(request, category_slug, product_slug):
     return render(request, 'store/product_detail.html', context)
 
 
-# def search(request):
-#     if 'keyword' in request.GET:
-#         keyword = request.GET['keyword']
-#         if keyword:
-#             products = Product.objects.order_by('-created_date').filter(
-#                 Q(description__icontains=keyword) | Q(product_name__icontains=keyword))
-#             product_count = products.count()
-#     context = {
-#         'products': products,
-#         'product_count': product_count,
-#     }
-#     return render(request, 'store/store.html', context)
+def search(request):
+    if 'keyword' in request.GET:
+        keyword = request.GET['keyword']
+        if keyword:
+            products = Product.objects.order_by('-created_at').filter(
+                Q(description__icontains=keyword) | Q(product_name__icontains=keyword))
+            product_count = products.count()
+    context = {
+        'products': products,
+        'product_count': product_count,
+    }
+    return render(request, 'store/store.html', context)
 
 
 # def submit_review(request, product_id):
