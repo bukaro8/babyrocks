23800e6704ebadf8663eba11d91439b208df2aea
Victor Ramirez <vicdeveloper83@gmail.com>
2025-12-09 16:50:42 +0000

    VARIATION MANAGERF  model created

--- store/models.py
+++ store/models.py
@@ -42,18 +42,18 @@ class Product(models.Model):
         return count
 
 
+variation_category_choice = (
+    ('color', 'color'),
+    ('size', 'size'),
+)
+
+
 class VariationManager(models.Manager):
     def colors(self):
         return super(VariationManager, self).filter(variation_category='color', is_active=True)
 
     def sizes(self):
         return super(VariationManager, self).filter(variation_category='size', is_active=True)
-
-
-variation_category_choice = (
-    ('color', 'color'),
-    ('size', 'size'),
-)
 
 
 class Variation(models.Model):
