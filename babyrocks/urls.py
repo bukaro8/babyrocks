18d7861955e240bd0ffd98e23c357318c782b41d
Victor Ramirez <vicdeveloper83@gmail.com>
2025-12-09 17:25:08 +0000

    accounts urls

--- babyrocks/urls.py
+++ babyrocks/urls.py
@@ -26,7 +26,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
     path('cart/', include('carts.urls')),
-    path('store/', include('store.urls'))
+    path('store/', include('store.urls')),
+    path('accounts/', include('accounts.urls')),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
