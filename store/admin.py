9bb3c0ee1b8dbd47c18fb5e588e3f159b76e6cdb
Victor Ramirez <vicdeveloper83@gmail.com>
2025-12-10 19:35:45 +0000

    custom css created for reviews

--- store/admin.py
+++ store/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
-from .models import Product, Variation
-# from .models import Product, Variation, ReviewRating, ProductGallery
+from .models import Product, Variation, ReviewRating, ProductGallery
 # import admin_thumbnails
 
 
@@ -28,5 +27,5 @@ admin.site.register(Product, ProductAdmin)
 
 admin.site.register(Variation, VariationAdmin)
 
-# admin.site.register(ReviewRating)
+admin.site.register(ReviewRating)
 # admin.site.register(ProductGallery)
