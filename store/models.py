6cc2a6027750d1aa514068917ced2db8c06168b7
Victor Ramirez <vicdeveloper83@gmail.com>
2025-12-10 19:27:34 +0000

    Review rating model created

--- store/models.py
+++ store/models.py
@@ -70,6 +70,19 @@ class Variation(models.Model):
         return self.variation_value
 
 
+class ProductGallery(models.Model):
+    product = models.ForeignKey(
+        Product, default=None, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='store/products', max_length=255)
+
+    def __str__(self):
+        return self.product.product_name
+
+    class Meta:
+        verbose_name = 'productgallery'
+        verbose_name_plural = 'product gallery'
+
+
 class ReviewRating(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     user = models.ForeignKey(Account, on_delete=models.CASCADE)
@@ -83,16 +96,3 @@ class ReviewRating(models.Model):
 
     def __str__(self):
         return self.subject
-
-
-class ProductGallery(models.Model):
-    product = models.ForeignKey(
-        Product, default=None, on_delete=models.CASCADE)
-    image = models.ImageField(upload_to='store/products', max_length=255)
-
-    def __str__(self):
-        return self.product.product_name
-
-    class Meta:
-        verbose_name = 'productgallery'
-        verbose_name_plural = 'product gallery'
