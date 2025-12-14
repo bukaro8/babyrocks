8dae4228e1dd2f596eb8f0ed79d7597a4823657a
Victor Ramirez <vicdeveloper83@gmail.com>
2025-12-14 21:54:23 +0000

    media changed1

--- babyrocks/urls.py
+++ babyrocks/urls.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from django.urls import path, include
-from . import views
+from django.urls import path, include, re_path
 from django.conf import settings
-from django.conf.urls.static import static
+from django.views.static import serve
+from . import views
 from honeypot.views import fake_admin
 
 urlpatterns = [
@@ -15,5 +15,8 @@ urlpatterns = [
     path("orders/", include("orders.urls")),
 ]
 
-
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# Serve uploaded media even when DEBUG=False
+urlpatterns += [
+    re_path(r"^media/(?P<path>.*)$", serve,
+            {"document_root": settings.MEDIA_ROOT}),
+]
